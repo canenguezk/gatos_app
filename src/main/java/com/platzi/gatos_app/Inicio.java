bc9629e8267f4be83e096d3a441ae089f3ed7f4a
Karen Cañenguez <canenguezk@gmail.com>
2020-12-02 15:02:57 -0600

    Iniciando proyecto Gatos app

---
+++ src/main/java/com/platzi/gatos_app/Inicio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.gatos_app;
+
+/**
+ *
+ * @author Karen
+ */
+public class Inicio {
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
